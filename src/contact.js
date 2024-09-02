9c1e1f4c260ada556dbec12af5b1a87614113ed3
Ndamulelo <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2024-09-02 07:02:48 +0200

    add contact page

---
+++ src/contact.js
@@ -0,0 +1,51 @@
+import "./styles/contact.css";
+import Picture from "./images/pexels-zdmit-6851933.jpg";
+
+export default function Contact() {
+  const content = document.querySelector("#content");
+
+  //create contact container element
+  const contactContainer = document.createElement("div");
+  contactContainer.classList.add("contactSection");
+
+  //contact heading
+  const contactHeading = document.createElement("h2");
+  contactHeading.classList.add("contact-heading");
+  contactHeading.textContent = "Contact";
+
+  contactContainer.appendChild(contactHeading);
+
+  //create element for an image
+  const image = document.createElement("img");
+  image.classList.add("image");
+  image.src = Picture;
+  image.width = 500;
+
+  //contact info
+  const addressHeading = document.createElement("h2");
+  addressHeading.textContent = "Address";
+  const addressInfo = document.querySelector("p");
+
+  //create div for contact info
+  const infoContainer = document.createElement("div");
+  infoContainer.classList.add("info");
+  infoContainer.innerHTML = `
+  <div class="address">
+    <h2>Address</h2>
+    <p>Veale St, Nieuw Muckleneuk, Pretoria, 0181</p>
+  </div>
+   <div class="hours">
+     <h2>Hours</h2>
+     <p>09:00 - 17:00</p>
+     <p>Monday to Saturday</p>
+   </div>
+    <div class="contacts">
+      <h2>Contact Info</h2>
+      <p><span>Contact Number:</span>+29 721 632 827</p>
+      <p><span>Hours:</span>10:00 - 16:00</p>
+    </div>`;
+
+  contactContainer.appendChild(image);
+  contactContainer.appendChild(infoContainer);
+  content.appendChild(contactContainer);
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import "./styles/style.css";
 import Home from "./home";
 import Menu from "./menu";
+import Contact from "./contact";
 
 const homeBtn = document.querySelector("#home");
 const menuBtn = document.querySelector("#menu");
+const contactBtn = document.querySelector("#contact");
 const content = document.querySelector("#content");
-Menu();
+Contact();
 
 homeBtn.addEventListener("click", () => {
   content.innerHTML = "";
@@ -14,4 +16,9 @@ homeBtn.addEventListener("click", () => {
 menuBtn.addEventListener("click", () => {
   content.innerHTML = "";
   Menu();
+});
+
+contactBtn.addEventListener("click", () => {
+  content.innerHTML = ``;
+  Contact();
 });
